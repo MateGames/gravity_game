8656af986a489859bd8f94df0a921a6042595177
Mateo <mate.farago.07@gmail.com>
2024-10-19 19:04:42 +0200

    movmnet work, but more needed

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ pygame.init()
 
 
 # variables
-width = 1400  # 35
-height = 800  # 20
+width = 1400
+height = 700
 phat = __file__[:len(__file__)-len(os.path.basename(__file__))]
 FPS = 60
 
@@ -27,12 +27,12 @@ class Hitbox():
         self.color = BLUE if self.type == 'horiz' else PURPLE
         
     def draw(self):
-        pygame.draw.line(screen,self.color,self.start,self.end,2)
+        pygame.draw.line(screen,self.color,self.start,self.end,5)
         
         
 class Player():
     def __init__(self):
-        self.friction = .15
+        self.friction = .2
         self.accel_rate = .2
         self.max_speed = 6
         self.speed = 0
@@ -40,8 +40,9 @@ class Player():
         self.color = RED
         self.size = 50
         self.x =  width/2
-        self.y = height/2
+        self.y = 500-self.size
         
+        self.rect = pygame.Rect((self.x,self.y),(self.size,self.size))
 
     
     def move(self):
@@ -63,22 +64,32 @@ class Player():
                 
         
         self.x += self.speed
+        
+        for line in group:
+            if line.type == 'vert' and self.rect.clipline(line.start, line.end):
+                self.x -= self.speed*2
+                self.speed = -self.speed*.2
+        
 
         self.rect = pygame.Rect((self.x,self.y),(self.size,self.size))
         
             
     def draw(self):
         pygame.draw.rect(screen,self.color,self.rect,0)
-        print(round(self.speed,1),self.x)
+        #print(round(self.speed,1),self.x)
                         
 
 player = Player()
 
 group = []
-group.append(Hitbox((300,400),(300,100),'vert'))
-group.append(Hitbox((300,400),(100,400),'horiz'))
+group.append(Hitbox((1250,200),(1250,500),'vert'))
+group.append(Hitbox((150,200),(150,500),'vert'))
+group.append(Hitbox((150,200),(1250,200),'horiz'))
+group.append(Hitbox((150,500),(1250,500),'horiz'))
 
 
+img = pygame.image.load(f'{phat}\\src\\map0.png')
+img = pygame.transform.scale(img,(width,height))
 
 # screen
 screen = pygame.display.set_mode((width,height))
@@ -104,6 +115,7 @@ def main():
             break
         
         screen.fill(BLACK)
+        screen.blit(img,(0,0))
         
         
         for object in group:
@@ -113,7 +125,7 @@ def main():
         player.draw()
         
             
-        #print(pygame.mouse.get_pos())
+        print(pygame.mouse.get_pos())
 
         # pygame.display.flip()
         pygame.display.update()
